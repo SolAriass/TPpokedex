ff7fef58513a03b4c0ff62741c2d28fe7f5a9016
SolAriass <ssolariaas@gmail.com>
2025-04-23 23:40:39 -0300

    vista principal con imagenes cargadas y base de datos completa

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
+
 <?php
+
+//conexion base de datos pokedex
+
+$baseDeDatos = mysqli_connect('localhost', 'root', '', 'pokedex') or die("Error al conectar a la base de datos");
+
+//obtener datos
+
+$datos = mysqli_query($baseDeDatos, "SELECT * FROM pokemones");
+$pokemones= [] ;
+for ($i=0; $i < mysqli_num_rows($datos); $i++) {
+    $fila = mysqli_fetch_array($datos);
+  // echo "Numero: " . $fila["numero"] . ' - ' . "Nombre: " . $fila["nombre"] . ' - ' . "Tipo: " . $fila["tipo"] . ' - ' . "Descripcion: " . $fila["descripcion"] . ' - ' . "Region: " . $fila["region"] . "<br />";
+    $pokemones[] = $fila;
+}
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pokedex</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<div class="container mt-4">
+    <div class="container d-flex justify-content-between align-items-center my-4 flex-wrap gap-3">
+    <img src="logo.png" class="img-fluid rounded" style="width: 80px; height: auto;" alt="Logo">
+    <h1 class="mb-0 flex-grow-1 text-center fw-bolder">Pokedex</h1>
+    <button class="btn btn-outline-secondary">Login</button>
+    </div>
+    <form action="#" class="d-flex">
+        <input type="text" name="buscador" id="buscador" placeholder="busque a el pokemon que desee" class="form-control me-2">
+        <input type="submit" name="enviar" id="enviar" value="buscar" class="btn btn-primary">
+    </form>
+
+    <div class="row mt-4">
+        <?php foreach ($pokemones as $poke): ?>
+            <div class="col-md-4 mb-4">
+                <div class="card h-100 shadow d-flex flex-column">
+                    <h5 class="card-header text-center fw-bold bg-white border-bottom-0">
+                        #<?= $poke['numero'] ?> - <?= $poke['nombre'] ?>
+                    </h5>
+
+                    <div class="d-flex justify-content-center align-items-center flex-grow-1 p-3">
+                        <img src="<?= $poke['imagen'] ?>" class="img-fluid" style="max-height: 150px;">
+                    </div>
+
+                    <div class="card-body mt-auto">
+                        <p class="card-text"><strong>Tipo:</strong> <?= $poke['tipo'] ?></p>
+                        <p class="card-text"><strong>Región:</strong> <?= $poke['region'] ?></p>
+                        <p class="card-text"><strong>Descripción:</strong> <?= $poke['descripcion'] ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+</body>
+</html>
