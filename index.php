e96b75367584da4579093c671fc8fd9d83e192b1
nicolassbon <nicolasbon019@gmail.com>
2025-04-26 12:54:30 -0300

    se crea el login del usuario y el manejo de la session

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once 'config/ConexionBD.php';
+
 use config\ConexionBD;
 
 session_start();
@@ -57,9 +58,16 @@ if (isset($_GET['buscador']) && $_GET['buscador'] !== '') {
 
 <div class="container mt-4">
   <div class="container d-flex justify-content-between align-items-center my-4 flex-wrap gap-3">
-    <img src="logo.png" class="img-fluid rounded" style="width: 80px; height: auto;" alt="Logo">
+    <a href="index.php"><img src="logo.png" class="img-fluid rounded" style="width: 80px; height: auto;" alt="Logo"></a>
     <h1 class="mb-0 flex-grow-1 text-center fw-bold">Pokedex</h1>
-    <button class="btn btn-dark">Iniciar sesión</button>
+    <?php if (isset($_SESSION['usuario'])): ?>
+      <p class="text-center"><?php echo $_SESSION['usuario']; ?></p>
+      <form action="logout.php" method="post">
+        <button type="submit" class="btn btn-dark">Cerrar sesión</button>
+      </form>
+    <?php else : ?>
+      <a class="btn btn-dark" href="login.php">Iniciar sesión</a>
+    <?php endif; ?>
   </div>
 
   <!-- Formulario de búsqueda -->
@@ -72,6 +80,13 @@ if (isset($_GET['buscador']) && $_GET['buscador'] !== '') {
   <!-- Mensaje si no se encuentran resultados -->
   <?php if ($mensaje): ?>
     <div class="alert alert-warning mt-4 text-center"><?= $mensaje ?></div>
+  <?php endif; ?>
+
+  <!-- Botón para agregar un nuevo Pokémon -->
+  <?php if (isset($_SESSION['usuario'])): ?>
+    <div class="d-flex justify-content-center mt-4">
+      <a href="agregar_pokemon.php" class="btn btn-success">Agregar Nuevo Pokémon</a>
+    </div>
   <?php endif; ?>
 
   <!-- Mostrar los resultados de la búsqueda o todos los pokemones -->
@@ -90,6 +105,12 @@ if (isset($_GET['buscador']) && $_GET['buscador'] !== '') {
 
           <div class="card-footer mt-auto">
             <p class="card-text text-center"><?= $poke['descripcion'] ?></p>
+            <?php if (isset($_SESSION['usuario'])): ?>
+              <div class="container d-flex justify-content-around">
+                <button class="btn btn-outline-secondary">Eliminar</button>
+                <button class="btn btn-outline-secondary">Modificar</button>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
       </div>
