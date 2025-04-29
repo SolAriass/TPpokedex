34599bd518c99beb7621a4ae5877552346febdde
SolAriass <ssolariaas@gmail.com>
2025-04-29 20:54:44 -0300

    popup eliminar

--- crud/agregarPokemon.php
+++ crud/agregarPokemon.php
@@ -141,17 +141,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 
 
+<?php require("../footer/footer.php")
+?>
 
-<footer class="bg-dark text-white text-center py-3 mt-auto">
-    <div class="container">
-        <small>| &copy; <?= date("Y") ?> |
-            <small class="fw-bold">
-                Arias Sol - Bernacchia Julieta -
-                Bon Nicolás - De Oro Martin - Recchia, Javier
-            </small>
-            | Trabajo Práctico N°1 - Pokedex |</small>
-    </div>
-</footer>
 
 </body>
 </html>
